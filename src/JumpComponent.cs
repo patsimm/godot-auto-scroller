6b8a0781c2352efa859c1822e7258c5d8ec6f476
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2024-05-24 16:52:32 +0200

    add jump buffer

--- src/JumpComponent.cs
+++ src/JumpComponent.cs
@@ -24,11 +24,18 @@ public partial class JumpComponent : Component<CharacterBody2D> {
     [Export]
     private double _wallJumpCoolDown = 0.3;
 
+    [Export]
+    private double _jumpBufferTime = 0.15;
+
     private Vector2 _jumpDirection = Vector2.Zero;
 
     private Timer _wallJumpCoolDownDurationTimer = new();
 
     private Timer _jumpDurationTimer = new();
+
+    private Timer _jumpBufferTimer = new();
+
+    private bool _jumpActionActive;
 
     public override void _Ready() {
         base._Ready();
@@ -40,6 +47,10 @@ public partial class JumpComponent : Component<CharacterBody2D> {
         _wallJumpCoolDownDurationTimer.WaitTime = _wallJumpCoolDown;
         _wallJumpCoolDownDurationTimer.OneShot = true;
         AddChild(_wallJumpCoolDownDurationTimer);
+
+        _jumpBufferTimer.WaitTime = _jumpBufferTime;
+        _jumpBufferTimer.OneShot = true;
+        AddChild(_jumpBufferTimer);
     }
 
     public override string[] _GetConfigurationWarnings() {
@@ -55,25 +66,12 @@ public partial class JumpComponent : Component<CharacterBody2D> {
     }
 
     public void JumpStart() {
-        if (CoyoteTimeComponent?.CoyoteTimerCounter > 0f || Entity.IsOnFloor()) {
-            _jumpDurationTimer.Start();
-            _wallJumpCoolDownDurationTimer.Stop();
-            VelocityComponent.AddForce(Vector2.Up * _jumpAcceleration * 0.33f);
-        }
-
-        if (Entity.IsOnWallOnly() && _wallJumpCoolDownDurationTimer.TimeLeft <= 0) {
-            _jumpDurationTimer.Start();
-            _wallJumpCoolDownDurationTimer.Start();
-            MovementComponent.ReduceMovementSpeed(.1);
-            VelocityComponent.ResetVerticalVelocity();
-            var jumpDirection = (Vector2.Up + Entity.GetWallNormal()).Normalized();
-            VelocityComponent.AddForce(jumpDirection * _jumpAcceleration * 0.4f);
-        }
+        _jumpActionActive = true;
+        _jumpBufferTimer.Start();
     }
 
     public void JumpEnd() {
-        if (_jumpDurationTimer.TimeLeft > 0)
-            _jumpDurationTimer.Stop();
+        _jumpActionActive = false;
     }
 
     public override void _PhysicsProcess(double delta) {
@@ -82,10 +80,38 @@ public partial class JumpComponent : Component<CharacterBody2D> {
 
         if (Entity.IsOnCeiling()) {
             _jumpDurationTimer.Stop();
+            _jumpBufferTimer.Stop();
         }
 
+        var jumpForce = new Vector2();
+
         if (_jumpDurationTimer.TimeLeft > 0) {
-            VelocityComponent.AddForce(Vector2.Up * _jumpAcceleration * (float)delta);
+            jumpForce = Vector2.Up * _jumpAcceleration * (float)delta;
         }
+
+        if (_jumpBufferTimer.TimeLeft > 0 && _jumpDurationTimer.TimeLeft <= 0) {
+            if (CoyoteTimeComponent?.CoyoteTimerCounter > 0f || Entity.IsOnFloor()) {
+                _jumpDurationTimer.Start();
+                _jumpBufferTimer.Stop();
+                _wallJumpCoolDownDurationTimer.Stop();
+                jumpForce = Vector2.Up * _jumpAcceleration * 0.33f;
+            }
+
+            if (Entity.IsOnWallOnly() && _wallJumpCoolDownDurationTimer.TimeLeft <= 0) {
+                _jumpDurationTimer.Start();
+                _jumpBufferTimer.Stop();
+                _wallJumpCoolDownDurationTimer.Start();
+                MovementComponent.ReduceMovementSpeed(.1);
+                VelocityComponent.ResetVerticalVelocity();
+                var jumpDirection = (Vector2.Up + Entity.GetWallNormal()).Normalized();
+                jumpForce = jumpDirection * _jumpAcceleration * 0.4f;
+            }
+        }
+
+        if (_jumpDurationTimer.TimeLeft > 0 && !_jumpActionActive) {
+            _jumpDurationTimer.Stop();
+        }
+
+        VelocityComponent.AddForce(jumpForce);
     }
 }
