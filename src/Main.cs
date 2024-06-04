76b0fde99577202636638d3a981c1f889bd2efe8
Kai Rechtern <kairechtern@gmail.com>
2024-06-04 20:53:00 +0200

    use castle tilset for level two

--- src/Main.cs
+++ src/Main.cs
@@ -8,7 +8,7 @@ namespace Platformer;
 public partial class Main : Node2D {
     private sealed record Savegame(Dictionary<int, double> LevelHighScores);
 
-	public required Gui Gui { get; set; }
+    public required Gui Gui { get; set; }
     public required Hud Hud { get; set; }
     public required LevelLoaderComponent LevelLoaderComponent { get; set; }
     public required TimeCounter GameTimeCounter { get; set; }
@@ -20,37 +20,37 @@ public partial class Main : Node2D {
         Load();
     }
 
-	public override void _Ready() {
-		Gui = GetNode<Gui>("GUI");
-		Gui.PlayAgainButtonPressed += Restart;
+    public override void _Ready() {
+        Gui = GetNode<Gui>("GUI");
+        Gui.PlayAgainButtonPressed += Restart;
 
         Hud = GetNode<Hud>("HUD");
 
         GameTimeCounter = GetNode<TimeCounter>("GameTimeCounter");
 
         LevelLoaderComponent = GetNode<LevelLoaderComponent>("LevelLoaderComponent");
-		StartGame();
-	}
+        StartGame();
+    }
 
-	public void StartGame() {
+    public void StartGame() {
         GameTimeCounter.Reset();
-		Gui.Hide();
+        Gui.Hide();
         LevelLoaderComponent.LoadLevel(1);
-	}
+    }
 
-	public void Restart() {
-		StartGame();
-		GetTree().Paused = false;
-	}
+    public void Restart() {
+        StartGame();
+        GetTree().Paused = false;
+    }
 
-	public void OnLevelFailed() {
-		GetTree().Paused = true;
+    public void OnLevelFailed() {
+        GetTree().Paused = true;
         Gui.ShowGameOver();
         Gui.Show();
     }
 
-	public void OnLevelCompleted() {
-		GetTree().Paused = true;
+    public void OnLevelCompleted() {
+        GetTree().Paused = true;
         var score = GameTimeCounter.ElapsedTimeSeconds;
         var level = LevelLoaderComponent.LoadedLevel;
         if (!level.HasValue) throw new Exception($"Unable to execute {nameof(OnLevelCompleted)} if no level is loaded.");
@@ -58,9 +58,9 @@ public partial class Main : Node2D {
             _levelHighScores[level.Value] = score;
             Save();
         }
-		Gui.ShowLevelCompleted(score, _levelHighScores[level.Value]);
+        Gui.ShowLevelCompleted(score, _levelHighScores[level.Value]);
         Gui.Show();
-	}
+    }
 
     private void Save() {
         using var saveGame = FileAccess.Open("user://savegame.save", FileAccess.ModeFlags.Write);
